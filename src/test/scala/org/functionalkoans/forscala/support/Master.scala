1a408bcf75f34f9deeb3ff619b1445296f166584
Jan te Beest <jtb@woodwing.com>
2016-04-13 14:27:53 +0200

    build: Fix Stopper override

--- src/test/scala/org/functionalkoans/forscala/support/Master.scala
+++ src/test/scala/org/functionalkoans/forscala/support/Master.scala
@@ -2,27 +2,20 @@ package org.functionalkoans.forscala.support
 
 import org.scalatest.Stopper
 
-import scala.language.reflectiveCalls
-
 object Master extends Stopper {
-  var studentNeedsToMeditate = false
 
-  override def apply() = studentNeedsToMeditate
+  @volatile private[this] var studentNeedsToMeditate = false
 
-  type HasTestNameAndSuiteName = {
-    val suiteName: String
-    val testName: String
-  }
-
+  case class HasTestNameAndSuiteName(suiteName: String, testName: String)
 
   def studentFailed(event: HasTestNameAndSuiteName): String = {
     studentNeedsToMeditate = true
     meditationMessage(event)
   }
 
-  private def meditationMessage(event: HasTestNameAndSuiteName) = {
-    "Please meditate on koan \"%s\" of suite \"%s\"" format(event.testName, event.suiteName)
-  }
+  private def meditationMessage(event: HasTestNameAndSuiteName) =
+    s"""Please meditate on koan "${event.testName}" of suite "${event.suiteName}""""
 
+  override def stopRequested = studentNeedsToMeditate
+  override def requestStop() = studentNeedsToMeditate = true
 }
-
