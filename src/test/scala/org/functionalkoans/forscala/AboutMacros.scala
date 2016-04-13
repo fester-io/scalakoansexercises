cfac1d9029f181c827b196ee8c8eba50813a0c28
Jan te Beest <jtb@woodwing.com>
2016-04-13 15:26:24 +0200

    suites: Extend correct class and add (empty) AboutMacros suite

---
+++ src/test/scala/org/functionalkoans/forscala/AboutMacros.scala
@@ -0,0 +1,11 @@
+package org.functionalkoans.forscala
+
+import org.functionalkoans.forscala.support.KoanSuite
+import org.scalatest.Matchers
+
+
+/** Macro exercises */
+class AboutMacros extends KoanSuite with Matchers {
+
+
+}
--- src/test/scala/org/functionalkoans/forscala/Koans.scala
+++ src/test/scala/org/functionalkoans/forscala/Koans.scala
@@ -3,59 +3,59 @@ package org.functionalkoans.forscala
 import org.functionalkoans.forscala.support.Master
 import org.scalatest._
 
-class Koans extends Suite {
-  override def nestedSuites = List(
-    new AboutAsserts,
-    new AboutValAndVar,
-    new AboutLiteralBooleans,
-    new AboutLiteralNumbers,
-    new AboutLiteralStrings,
-    new AboutMethods,
-    new AboutClasses,
-    new AboutUniformAccessPrinciple,
-    new AboutConstructors,
-    new AboutParentClasses,
-    new AboutOptions,
-    new AboutObjects,
-    new AboutApply,
-    new AboutTuples,
-    new AboutHigherOrderFunctions,
-    new AboutEmptyValues,
-    new AboutLists,
-    new AboutMaps,
-    new AboutSets,
-    new AboutFormatting,
-    new AboutStringInterpolation,
-    new AboutPatternMatching,
-    new AboutCaseClasses,
-    new AboutRange,
-    new AboutPartiallyAppliedFunctions,
-    new AboutPartialFunctions,
-    new AboutImplicits,
-    new AboutTraits,
-    new AboutForExpressions,
-    new AboutInfixPrefixAndPostfixOperators,
-    new AboutInfixTypes,
-    new AboutMutableMaps,
-    new AboutMutableSets,
-    new AboutSequencesAndArrays,
-    new AboutIterables,
-    new AboutTraversables,
-    new AboutNamedAndDefaultArguments,
-    new AboutTypeTags,
-    new AboutPreconditions,
-    new AboutExtractors,
-    new AboutByNameParameter,
-    new AboutRepeatedParameters,
-    new AboutTypeSignatures,
-    new AboutTypeVariance,
-    new AboutEnumerations
-  )
+class Koans extends Suites(
+  new AboutAsserts,
+  new AboutValAndVar,
+  new AboutLiteralBooleans,
+  new AboutLiteralNumbers,
+  new AboutLiteralStrings,
+  new AboutMethods,
+  new AboutClasses,
+  new AboutUniformAccessPrinciple,
+  new AboutConstructors,
+  new AboutParentClasses,
+  new AboutOptions,
+  new AboutObjects,
+  new AboutApply,
+  new AboutTuples,
+  new AboutHigherOrderFunctions,
+  new AboutEmptyValues,
+  new AboutLists,
+  new AboutMaps,
+  new AboutSets,
+  new AboutFormatting,
+  new AboutStringInterpolation,
+  new AboutPatternMatching,
+  new AboutCaseClasses,
+  new AboutRange,
+  new AboutPartiallyAppliedFunctions,
+  new AboutPartialFunctions,
+  new AboutImplicits,
+  new AboutTraits,
+  new AboutForExpressions,
+  new AboutInfixPrefixAndPostfixOperators,
+  new AboutInfixTypes,
+  new AboutMutableMaps,
+  new AboutMutableSets,
+  new AboutSequencesAndArrays,
+  new AboutIterables,
+  new AboutTraversables,
+  new AboutNamedAndDefaultArguments,
+  new AboutTypeTags,
+  new AboutPreconditions,
+  new AboutExtractors,
+  new AboutByNameParameter,
+  new AboutRepeatedParameters,
+  new AboutTypeSignatures,
+  new AboutTypeVariance,
+  new AboutEnumerations,
+  new AboutMacros) {
 
-  override def run(
-      testName: Option[String], reporter: Reporter, stopper: Stopper, filter: Filter,
-      configMap: Map[String, Any], distributor: Option[Distributor], tracker: Tracker) {
-    super.run(testName, reporter, Master, filter, configMap, distributor, tracker)
+  override def run(testName: Option[String], args: Args) = {
+    super.run(testName, args.copy(stopper = Master))
   }
 
+  override protected def runTest(testName: String, args: Args) = {
+    super.runTest(testName, args.copy(stopper = Master))
+  }
 }
