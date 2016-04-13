95c52f026fdd2558eec07e60206b7a8ab8b81f88
Jan te Beest <jtb@woodwing.com>
2016-04-13 20:27:48 +0200

    test/reporter: Use `note` instead of `info` as info won't print what we want (note is new in respect to original project)

--- src/test/scala/org/functionalkoans/forscala/support/KoanSuite.scala
+++ src/test/scala/org/functionalkoans/forscala/support/KoanSuite.scala
@@ -6,59 +6,50 @@ import org.scalatest.events._
 import org.scalatest.exceptions.TestPendingException
 import org.scalatest.matchers.BeMatcher
 
+
+object KoanSuite extends KoanSuite
+
+
 trait KoanSuite extends FunSuite with Matchers {
 
-  def koan(name: String)(fun: => Unit) {
-    test(name.stripMargin('|'))(fun)
-  }
+  def koan(name: String)(fun: => Unit) = test(name.stripMargin('|'))(fun)
 
   def meditate() = pending
 
-  def __ : BeMatcher[Any] = {
-    throw new TestPendingException
-  }
+  def __ : BeMatcher[Any] = throw new TestPendingException
 
   protected class ___ extends Exception {
     override def toString = "___"
   }
 
-  private class ReportToTheMaster(other: Reporter) extends Reporter {
-    var failed = false
-    def failure(event: Master.HasTestNameAndSuiteName) {
+  class ReportToTheMaster(other: Reporter) extends Reporter {
+    @volatile var failed = false
+    def failure(event: HasTestNameAndSuiteName) = {
       failed = true
-      info("*****************************************")
-      info("*****************************************")
-      info("")
-      info("")
-      info("")
-      info(Master.studentFailed(event))
-      info("")
-      info("")
-      info("")
-      info("*****************************************")
-      info("*****************************************")
+      note("*****************************************")
+      note("*****************************************")
+      note("")
+      note("")
+      note("")
+      note(Master.studentFailed(event))
+      note("")
+      note("")
+      note("")
+      note("*****************************************")
+      note("*****************************************")
     }
 
-    def apply(event: Event) {
-      event match {
-        case e: TestIgnored ⇒ failure(HasTestNameAndSuiteName(e.testName, e.suiteName))
-        case e: TestFailed ⇒ failure(HasTestNameAndSuiteName(e.testName, e.suiteName))
-        case e: TestPending ⇒ failure(HasTestNameAndSuiteName(e.testName, e.suiteName))
-        case _ ⇒ other(event)
-      }
-
+    def apply(event: Event) = event match {
+      case e: TestIgnored ⇒ failure(HasTestNameAndSuiteName(e.suiteName, e.testName))
+      case e: TestFailed ⇒ failure(HasTestNameAndSuiteName(e.suiteName, e.testName))
+      case e: TestPending ⇒ failure(HasTestNameAndSuiteName(e.suiteName, e.testName))
+      case _ ⇒ other(event)
     }
   }
-
-  override def run(testName: Option[String], args: Args) =
-    if (!Master.stopRequested)
-      super.run(testName, args.copy(reporter = new ReportToTheMaster(args.reporter), stopper = Master))
-    else
-      FailedStatus
 
   override protected def runTest(testName: String, args: Args) =
     if (!Master.stopRequested)
       super.runTest(testName, args.copy(reporter = new ReportToTheMaster(args.reporter), stopper = Master))
     else
-      FailedStatus
+      SucceededStatus
 }
