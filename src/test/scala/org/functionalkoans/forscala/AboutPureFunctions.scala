ec0fbcbb83235f724e792667989f3ab977a8bf67
Jan te Beest <jtb@woodwing.com>
2016-04-19 07:59:25 +0200

    =suites: AboutPureFunctions stub

---
+++ src/test/scala/org/functionalkoans/forscala/AboutPureFunctions.scala
@@ -0,0 +1,20 @@
+package org.functionalkoans.forscala
+
+import org.functionalkoans.forscala.support.KoanSuite
+import org.scalatest.Matchers
+
+
+/** = Pure functions =
+  * For these exercises we will only be using ''pure'' functions. These are functions that take one or more arguments
+  * and return an argument. Pure functions can not change state!
+  * <p>To enforce the use of pure functions, we will be writing them in an object (let's call it a module) and we
+  * will not keep ''any'' state.
+  */
+class AboutPureFunctions extends KoanSuite with Matchers {
+
+}
+
+
+object PureFunctionModule {
+
+}
